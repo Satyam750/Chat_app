a4f83d34d744b91c1f7241d408319fb8412a22b8
Satyam750 <satyamthakur05@gmail.com>
2024-05-09 22:11:59 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import path from "path";
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+import connectToMongoDB from "./db/connectToMongoDB.js"
+
+import {app,server} from "./socket/socket.js"
+
+dotenv.config();
+
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api/auth',authRoutes)
+app.use('/api/message',messageRoutes)
+app.use('/api/users',userRoutes)
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
+
+// app.get("/",(req,res)=>{
+//     // route http://localhost:5000
+//     res.send("Server is Ready !")
+// })
+
+server.listen(PORT, ()=>{
+    connectToMongoDB()
+    console.log(`Server is running on port ${PORT}`)
+});
